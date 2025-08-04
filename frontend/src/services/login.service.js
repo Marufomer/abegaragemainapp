f1da0d34045189a1f3d8319e57311106fcf3eb4f
Marufomer <omermaruf07@gmail.com>
2025-08-04 10:41:42 +0300

    updated

---
+++ frontend/src/services/login.service.js
@@ -0,0 +1,21 @@
+const api_url = import.meta.env.VITE_API_URL;
+console.log(api_url)
+
+// A function to send the login request to the server
+const logIn = async (formData) => {
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+  };
+  console.log("About to send request");
+  console.log(requestOptions.body);
+  const response = await fetch(`${api_url}/api/employee/login`, requestOptions);
+  return response;
+};
+
+// Export the functions
+const logInService = {
+  logIn,
+};
+export default logInService;
